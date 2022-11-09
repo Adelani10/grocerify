3b2e8f32179d9ca1dccb8f980f3ad4a5dd8784d8
Ade Lani <ade@example.com>
2022-11-08 22:44:22 -0800

    minor edit

--- src/pages/stats.jsx
+++ src/pages/stats.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Stats () {
     return (
-        <section className="h-screen">Hello Stats</section>
+        <section className="h-screen"> Stats Page </section>
     )
 }
